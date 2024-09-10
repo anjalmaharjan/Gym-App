c78269c713d4626144c0db7cee3cd73a35f61d7a
anjalmaharjan <heromaharjan@gmail.com>
2024-09-10 22:12:56 +0545

    Initial Commit

---
+++ Gym-App/Gym_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_AppApp.swift
+//  Gym-App
+//
+//  Created by Anjal Maharjan on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Gym_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
